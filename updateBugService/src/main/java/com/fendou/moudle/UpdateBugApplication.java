b5cb0265fd50512f7d727a0d0e8bf6a1c2ad0692
f <f>
2020-05-13 11:48:55 +0800

    7

--- updateBugService/src/main/java/com/fendou/moudle/UpdateBugApplication.java
+++ updateBugService/src/main/java/com/fendou/moudle/UpdateBugApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication(scanBasePackages = {"com.fendou.moudle"})
-@MapperScan("com.fendou.com.fendou.moudle.mapper")
+@MapperScan("com.fendou.moudle.mapper")
 public class UpdateBugApplication {
 
     public static void main(String[] args){
