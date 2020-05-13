7a4948fe1839ee8c5f2ca2cde7a82e728d4db36d
f <f>
2020-05-13 09:54:38 +0800

    2

--- updateBugService/src/main/java/com/fendou/moudle/UpdateBugApplication.java
+++ updateBugService/src/main/java/com/fendou/moudle/UpdateBugApplication.java
@@ -4,7 +4,7 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = {"com.fendou.com.fendou.moudle"})
+@SpringBootApplication(scanBasePackages = {"com.fendou.moudle"})
 @MapperScan("com.fendou.com.fendou.moudle.mapper")
 public class UpdateBugApplication {
 
